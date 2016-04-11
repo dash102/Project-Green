ecc99f7d3fbf582d85708065671611f95a0b98ea
Stephanie Fu <stephaniefu@rocketmail.com>
2016-04-10 20:29:25 -0500

    Update home_page.java

--- GoogleMapsApp/app/src/main/java/mhs/team/googlemapsapp/home_page.java
+++ GoogleMapsApp/app/src/main/java/mhs/team/googlemapsapp/home_page.java
@@ -16,8 +16,6 @@ public class home_page extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         //getActionBar().setTitle("Recycle Home");
 
-        // [Optional] Power your app with Local Datastore. For more info, go to
-        // https://parse.com/docs/android/guide#local-datastore
         Parse.enableLocalDatastore(this);
 
         Parse.initialize(this);
